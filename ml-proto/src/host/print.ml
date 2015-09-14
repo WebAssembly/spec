a4ab03b3ae193a9ce9f6e963e2c073f772e35362
Luke Wagner <mail@lukewagner.name>
2015-09-14 12:25:28 -0500

    Addressed second round of comments

--- ml-proto/src/host/print.ml
+++ ml-proto/src/host/print.ml
@@ -13,7 +13,7 @@ open Types
 
 let func_type f =
   let {Ast.params; result; _} = f.it in
-  {ins = List.map Source.it params; out = Lib.Option.map it result}
+  {ins = List.map it params; out = Lib.Option.map it result}
 
 let string_of_table_type = function
   | None -> "()"
