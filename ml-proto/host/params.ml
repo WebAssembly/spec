c38d5d1e1381b8bf83daceebc9002917f6624089
Dan Gohman <sunfish@mozilla.com>
2015-10-14 15:38:09 -0700

    Add an always-supported feature, and a test for it.

--- ml-proto/host/params.ml
+++ ml-proto/host/params.ml
@@ -1,4 +1,7 @@
 let page_size = 4096L
 
-(* For now, return false for all features. *)
-let has_feature = fun str -> false
+let has_feature = fun str -> match str with
+  (* We always support this feature :-). *)
+  | "wasm" -> true
+  (* If we don't recognize a feature name, we don't support the feature. *)
+  | _ -> false
