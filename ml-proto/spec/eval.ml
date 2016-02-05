81b1a30349b86e092918352c7f63baf6de6841b3
Dan Gohman <sunfish@mozilla.com>
2016-02-04 16:35:04 -0800

    Fix an unused variable.

--- ml-proto/spec/eval.ml
+++ ml-proto/spec/eval.ml
@@ -157,7 +157,7 @@ let rec eval_expr (c : config) (e : expr) =
   | Br_if (eo, e, x) ->
     let i = int32 (eval_expr c e) e.at in
     let v = eval_expr_opt c eo in
-    if i <> 0l then raise (label c x (eval_expr_opt c eo)) else None
+    if i <> 0l then raise (label c x v) else None
 
   | If (e1, e2, e3) ->
     let i = int32 (eval_expr c e1) e1.at in
