7847625467377260c4f63d47525831540e47228d
rossberg-chromium <rossberg@chromium.org>
2016-08-30 12:00:14 +0200

    Forgot to add file

---
+++ ml-proto/spec/instance.ml
@@ -0,0 +1,29 @@
+open Values
+
+module ExportMap = Map.Make(String)
+
+type global = value ref
+
+type func =
+  | AstFunc of instance ref * Kernel.func
+  | HostFunc of Types.func_type * (value list -> value option)
+
+and extern =
+  | ExternalFunc of func
+  | ExternalTable of Table.t
+  | ExternalMemory of Memory.t
+  | ExternalGlobal of global
+
+and instance =
+{
+  module_ : Kernel.module_;
+  funcs : func list;
+  tables : Table.t list;
+  memories : Memory.t list;
+  globals : global list;
+  exports : extern ExportMap.t;
+}
+
+let instance m =
+  { module_ = m; funcs = []; tables = []; memories = []; globals = [];
+    exports = ExportMap.empty }
