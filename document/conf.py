565d4ee6fd84a7513f59c491446b7fe912c51a83
Andreas Rossberg <rossberg@chromium.org>
2016-11-23 17:15:37 +0100

    Hack relative PDF path

--- document/conf.py
+++ document/conf.py
@@ -142,7 +142,7 @@ html_theme_options = {
   'sidebar_link_underscore': '#FFFFFF',
   'show_powered_by': False,
   'extra_nav_links': {
-    'Download as PDF': '_static/' + name + '.pdf'
+    'Download as PDF': 'DOWNLOADDIR/' + name + '.pdf'
   },
 }
 
@@ -181,7 +181,7 @@ html_logo = logo
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'custom.css']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
