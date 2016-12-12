4e0abc8fd90848ee69f3adda9994333776ad8c67
rossberg-chromium <rossberg@chromium.org>
2016-12-12 14:56:33 +0100

    Eps

--- interpreter/spec/eval.mli
+++ interpreter/spec/eval.mli
@@ -5,5 +5,5 @@ exception Link of Source.region * string
 exception Trap of Source.region * string
 exception Crash of Source.region * string
 
-val init : Ast.module_ -> extern list -> instance
+val init : Ast.module_ -> extern list -> instance (* raises Link, Trap *)
 val invoke : closure -> value list -> value list (* raises Trap *)
