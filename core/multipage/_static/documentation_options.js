29a0c9dfcdb2c0356f4705699ba4eeb0b973a710
Travis CI <noreply@webassembly.org>
2018-02-12 23:41:21 +0000

    Deploy to GitHub Pages: 9b5f6620994c253084137bbc825af911eea47930

---
+++ core/multipage/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '1.0',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt'
+};
