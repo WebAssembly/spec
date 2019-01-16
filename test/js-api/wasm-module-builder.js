51dd545cec15ad9d0012d82ffca9d4c45d4c45ab
Sven Sauleau <github@sauleau.com>
2019-01-16 17:10:00 +0100

    Update wasm-module-builder.js

--- test/js-api/wasm-module-builder.js
+++ test/js-api/wasm-module-builder.js
@@ -74,7 +74,9 @@ class Binary extends Array {
     // Emit section length.
     this.emit_u32v(section.length);
     // Copy the temporary buffer.
-    this.push(...section);
+    for (let i = 0, len = section.length; i < len; i++) {
+      this.push(section[i]);
+    }
   }
 }
 
