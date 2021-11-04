ec10d317fdd4986b347c23ba58eec120a5bc56a2
Ng Zhi An <zhin@google.com>
2021-11-04 15:42:36 -0700

    WIP try using sphinx to generate w3c document
    This uses a multihtml Sphinx builder, together with custom HTML layout
    (layout.html and page.html), to build a document that matches w3c
    requirements.
    
    Currently the toctree directive does not work right, so it is duplicated
    in the side bar (https://github.com/sphinx-doc/sphinx/pull/9830). The
    W3C styles also don't apply fully to the side bar (because the Sphinx
    generated TOC doesn't have the right class name, but this might be fixed
    by some JS).
    
    layout.html is used for all pages, index + individual pages, so the
    front matter is guarded by a check that the current page is index.
    layout.html is rather large, most of it is the front matter copied from
    https://github.com/WebAssembly/spec/issues/1386#issuecomment-957347678.
    page.html is used by all the individual rst pages.
    
    Right now all the configuration lives in a new folder, newhtml, but it
    can presumably replace the singlehtml folder.

--- document/core/index.rst
+++ document/core/index.rst
@@ -10,16 +10,46 @@ WebAssembly Specification
    | Latest Draft: |WasmDraft|
    | Issue Tracker: |WasmIssues|
 
-.. toctree::
-   :maxdepth: 2
+.. only:: builder_singlehtml
 
-   intro/index
-   syntax/index
-   valid/index
-   exec/index
-   binary/index
-   text/index
-   appendix/index
+  .. toctree::
+     :caption: Table of Contents
+     :numbered:
+     :maxdepth: 3
+
+     intro/index
+     syntax/index
+     valid/index
+     exec/index
+     binary/index
+     text/index
+     appendix/index
+
+.. only:: builder_html and not newhtml
+
+  .. toctree::
+     :maxdepth: 2
+
+     intro/index
+     syntax/index
+     valid/index
+     exec/index
+     binary/index
+     text/index
+     appendix/index
+
+.. only:: builder_html and newhtml
+
+  .. toctree::
+     :maxdepth: 3
+
+     intro/index
+     syntax/index
+     valid/index
+     exec/index
+     binary/index
+     text/index
+     appendix/index
 
 .. only:: latex
 
---
+++ document/core/newhtml/conf.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Sphinx configuration for bikeshed builds (make bikeshed).
+# This imports the main document/core/conf.py, then overwrites certain config
+# values for the bikeshed build.
+
+import os
+import sys
+original_pwd = os.path.abspath('..')
+# Change sys.path so that we can find the main 'conf.py'.
+sys.path.insert(0, original_pwd)
+from conf import *
+main_conf_pwd = pwd
+
+# Now that we have imported all the settings, we need to overwrite some of
+# them.
+
+# The first is `pwd`, we want to reset it to document/core, because rst_prolog
+# below depends on this to find macros.def.
+pwd = original_pwd
+
+# Overwrite html themes and configurations.
+html_theme = 'basic'
+html_theme_options = {
+  # 'nosidebar': True, # use respec
+  'nosidebar': True,
+  'sidebarwidth': '260px',
+}
+html_css_files = ['https://www.w3.org/StyleSheets/TR/2016/W3C-ED']
+
+# Look for layout.html in this directory, it overwrites the underlying theme's
+# layout to remove some bits we don't need.
+templates_path = ['.']
+
+# Overwrite the prolog to make sure the include directive has the correct path.
+
+main_macros_def = "/" + main_conf_pwd + "/util/macros.def"
+# If we hit this assertion, the configuration files probably moved, or files
+# are renamed, and we have to update rst_prolog accordingly.
+assert(main_macros_def in rst_prolog)
+rst_prolog  = rst_prolog.replace(main_macros_def, "/" + pwd + "/util/macros.def")
+
+tags.add('newhtml')
