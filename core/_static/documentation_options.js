b8dbbd5572fd350f3080472ccb3d6f81dfbd06dc
ngzhian <ngzhian@users.noreply.github.com>
2021-11-18 18:51:50 +0000

    deploy: f5a260a2025ba4d7d398654581c7c532e3a2c319

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 (Draft 2021-11-16)',
+    VERSION: '1.1 (Draft 2021-11-18)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
