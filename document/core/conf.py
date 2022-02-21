64cb3bd5249d6a35585636bd5ba5321197de3160
Yuri Iozzelli <y.iozzelli@gmail.com>
2022-02-21 16:24:28 +0100

    Fix conf for branch hinting proposl

--- document/core/conf.py
+++ document/core/conf.py
@@ -66,10 +66,10 @@ editor = u'Andreas Rossberg (editor)'
 logo = 'static/webassembly.png'
 
 # The name of the GitHub repository this resides in
-repo = 'annotations'
+repo = 'branch-hinting'
 
 # The name of the proposal it represents, if any
-proposal = 'custom annotations'
+proposal = 'branch hinting'
 
 # The draft version string (clear out for release cuts)
 draft = ' (Draft ' + date.today().strftime("%Y-%m-%d") + ')'
