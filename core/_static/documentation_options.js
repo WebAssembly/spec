353286c71fefd955a41721cd0d4121ac8b26b196
dschuff <dschuff@users.noreply.github.com>
2022-04-16 00:52:55 +0000

    deploy: ce4b6c4d47eb06098cc7ab2e81f24748da822f20

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 (Draft 2022-04-15)',
+    VERSION: '2.0 (Draft 2022-04-16)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
