dbabef2245149dd34562afd122fd766f3976c601
Asumu Takikawa <asumu@igalia.com>
2022-07-18 17:00:35 -0700

    [interpreter]: address PR feedback

--- interpreter/meta/jslib/wast.ml
+++ interpreter/meta/jslib/wast.ml
@@ -1,9 +1,7 @@
+(* Implements a wrapper library that allows the use of the reference
+ * interpreter's encode/decode functionality in JavaScript.
+ *)
 open Js_of_ocaml
-
-let decode (buf : Typed_array.arrayBuffer Js.t) width : (Js.js_string Js.t) =
-  let s = Typed_array.String.of_uint8Array (new%js Typed_array.uint8Array_fromBuffer buf) in
-  let m = Decode.decode "(decode)" s in
-  Js.string (Sexpr.to_string width (Arrange.module_ m))
 
 let _ =
   Js.export "WebAssemblyText"
@@ -14,12 +12,15 @@ let _ =
         let bs =
           match def.Source.it with
           | Script.Textual m -> (Encode.encode m)
-          | Script.Encoded (_, bs) -> bs in
+          | Script.Encoded (_, bs) -> bs
+          | Script.Quoted (_, _) -> failwith "Unsupported" in
         let buf = new%js Typed_array.arrayBuffer (String.length bs) in
         let u8arr = new%js Typed_array.uint8Array_fromBuffer buf in
         String.iteri (fun i c -> Typed_array.set u8arr i (int_of_char c)) bs; buf
 
-      method decode buf = decode buf 80
-      method decode2 buf width = decode buf width
+      method decode (buf : Typed_array.arrayBuffer Js.t) width : (Js.js_string Js.t) =
+        let s = Typed_array.String.of_uint8Array (new%js Typed_array.uint8Array_fromBuffer buf) in
+        let m = Decode.decode "(decode)" s in
+        Js.string (Sexpr.to_string width (Arrange.module_ m))
 
     end)
