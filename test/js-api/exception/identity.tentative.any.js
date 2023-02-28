585c7da72e481a257ff7192679584bb812dc1555
Heejin Ahn <aheejin@gmail.com>
2023-02-27 17:17:42 -0800

    [js-api] Test Wasm-defined tag in rethrow identity test (#265)
    This addresses
    https://github.com/WebAssembly/exception-handling/pull/253#issuecomment-1433703216
    by adding a set of tests that use a Wasm-defined and exported tag.

--- test/js-api/exception/identity.tentative.any.js
+++ test/js-api/exception/identity.tentative.any.js
@@ -13,9 +13,19 @@ test(() => {
   const jsTagExnDiffPayload = new WebAssembly.Exception(jsTag, [53]);
   const throwJSTagExnIndex = builder.addImport("module", "throwJSTagExn", kSig_v_v);
 
+  // Tag defined in Wasm and exported to JS
+  const wasmTagIndex = builder.addTag(kSig_v_i);
+  builder.addExportOfKind("wasmTag", kExternalTag, wasmTagIndex);
+  const throwWasmTagExnIndex = builder.addImport("module", "throwWasmTagExn", kSig_v_v);
+  // Will be assigned after an instance is created
+  let wasmTagExn = null;
+  let wasmTagExnSamePayload = null;
+  let wasmTagExnDiffPayload = null;
+
   const imports = {
     module: {
       throwJSTagExn: function() { throw jsTagExn; },
+      throwWasmTagExn: function() { throw wasmTagExn; },
       jsTag: jsTag
     }
   };
@@ -28,6 +38,20 @@ test(() => {
       kExprTry, kWasmStmt,
         kExprCallFunction, throwJSTagExnIndex,
       kExprCatch, jsTagIndex,
+        kExprDrop,
+        kExprRethrow, 0x00,
+      kExprEnd
+    ])
+    .exportFunc();
+
+  // Call a JS function that throws an exception using a Wasm-defined tag,
+  // catches it with a 'catch' instruction, and rethrows it.
+  builder
+    .addFunction("catch_wasm_tag_rethrow", kSig_v_v)
+    .addBody([
+      kExprTry, kWasmStmt,
+        kExprCallFunction, throwWasmTagExnIndex,
+      kExprCatch, wasmTagIndex,
         kExprDrop,
         kExprRethrow, 0x00,
       kExprEnd
@@ -47,11 +71,24 @@ test(() => {
     ])
     .exportFunc();
 
+  // Call a JS function that throws an exception using a Wasm-defined tag,
+  // catches it with a 'catch_all' instruction, and rethrows it.
+  builder
+    .addFunction("catch_all_wasm_tag_rethrow", kSig_v_v)
+    .addBody([
+      kExprTry, kWasmStmt,
+        kExprCallFunction, throwWasmTagExnIndex,
+      kExprCatchAll,
+        kExprRethrow, 0x00,
+      kExprEnd
+    ])
+    .exportFunc();
+
   const buffer = builder.toBuffer();
 
-  // The exception object's identity should be preserved across 'rethrow's in
-  // Wasm code. Do tests with a tag defined in JS.
   WebAssembly.instantiate(buffer, imports).then(result => {
+    // The exception object's identity should be preserved across 'rethrow's in
+    // Wasm code. Do tests with a tag defined in JS.
     try {
       result.instance.exports.catch_js_tag_rethrow();
     } catch (e) {
@@ -67,6 +104,26 @@ test(() => {
       assert_equals(e, jsTagExn);
       assert_not_equals(e, jsTagExnSamePayload);
       assert_not_equals(e, jsTagExnDiffPayload);
+    }
+
+    // Do the same tests with a tag defined in Wasm.
+    const wasmTag = result.instance.exports.wasmTag;
+    wasmTagExn = new WebAssembly.Exception(wasmTag, [42]);
+    wasmTagExnSamePayload = new WebAssembly.Exception(wasmTag, [42]);
+    wasmTagExnDiffPayload = new WebAssembly.Exception(wasmTag, [53]);
+    try {
+      result.instance.exports.catch_wasm_tag_rethrow();
+    } catch (e) {
+      assert_equals(e, wasmTagExn);
+      assert_not_equals(e, wasmTagExnSamePayload);
+      assert_not_equals(e, wasmTagExnDiffPayload);
+    }
+    try {
+      result.instance.exports.catch_all_wasm_tag_rethrow();
+    } catch (e) {
+      assert_equals(e, wasmTagExn);
+      assert_not_equals(e, wasmTagExnSamePayload);
+      assert_not_equals(e, wasmTagExnDiffPayload);
     }
   });
 }, "Identity check");
