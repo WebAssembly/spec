dcaf9d71f5a9661c0325b2715034472d26256de7
Andreas Rossberg <rossberg@mpi-sws.org>
2023-03-26 14:09:56 +0200

    Splicing

--- spectec/src/exe-watsup/main.ml
+++ spectec/src/exe-watsup/main.ml
@@ -6,16 +6,21 @@ let version = "0.2"
 let banner () =
   print_endline (name ^ " " ^ version ^ " generator")
 
-let usage = "Usage: " ^ name ^ " [option] [file ...]"
+let usage = "Usage: " ^ name ^ " [option] [file ...] [-p file ...]"
 
-let args = ref []
-let add_arg source = args := !args @ [source]
-let out_arg = ref ""
+let dst = ref false
+let srcs = ref []
+let dsts = ref []
+let odst = ref ""
+
+let add_arg source =
+  let args = if !dst then dsts else srcs in args := !args @ [source]
 
 let argspec = Arg.align
 [
   "-v", Arg.Unit banner, " Show version";
-  "-o", Arg.String (fun s -> out_arg := s), " Generate file";
+  "-o", Arg.String (fun s -> odst := s), " Generate file";
+  "-p", Arg.Set dst, " Patch files";
   "-help", Arg.Unit ignore, "";
   "--help", Arg.Unit ignore, "";
 ]
@@ -24,13 +29,7 @@ let error at msg =
   prerr_endline (Source.string_of_region at ^ ": " ^ msg);
   exit 1
 
-let trace s = if !out_arg = "" then Printf.printf "== %s\n%!" s
-
-let write_out latex =
-  let oc = open_out !out_arg in
-  Fun.protect (fun () -> output_string oc latex)
-    ~finally:(fun () -> close_out oc)
-
+let trace s = if !odst = "" then Printf.printf "== %s\n%!" s
 
 let parse_file file =
   try
@@ -43,21 +42,23 @@ let () =
   try
     Arg.parse argspec add_arg usage;
     trace "Parsing...";
-    let el = List.concat_map parse_file !args in
+    let el = List.concat_map parse_file !srcs in
     trace "Multiplicity checking...";
     Frontend.Multiplicity.check el;
     trace "Elaboration...";
     let il = Frontend.Elab.elab el in
     trace "Printing...";
-    if !out_arg = "" then Printf.printf "%s\n%!" (Il.Print.string_of_script il);
+    if !odst = "" && !dsts = [] then
+      Printf.printf "%s\n%!" (Il.Print.string_of_script il);
     trace "IL Validation...";
     Il.Validation.valid il;
     trace "Latex Generation...";
-    let latex = Backend_latex.Render.(render_script (env config) el) in
-    if !out_arg = "" then
-      print_endline latex
-    else
-      write_out latex;
+    if !odst = "" && !dsts = [] then
+      print_endline (Backend_latex.Gen.gen_string el);
+    if !odst <> "" then
+      Backend_latex.Gen.gen_file !odst el;
+    let env = Backend_latex.Splice.(env config_latex el) in
+    List.iter (Backend_latex.Splice.splice_file env) !dsts;
     trace "Complete."
   with
   | Source.Error (at, msg) ->
