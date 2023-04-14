f0e905c1c4b6a1db8bbfe77a18950c124d4d1aa0
Wonho Shin <wonhoshin@iMac-4.local>
2023-04-14 14:22:39 +0900

    Add structured stringifier for il

--- spectec/src/il/print.ml
+++ spectec/src/il/print.ml
@@ -242,3 +242,317 @@ let rec string_of_def d =
 
 let string_of_script ds =
   concat "" (List.map (suffix string_of_def "\n") ds)
+
+
+let sprintf = Printf.sprintf
+
+let structured_string_of_list stringifier = function
+  | [] -> "[]"
+  | h :: t ->
+      "[" ^
+        List.fold_left
+          (fun acc elem -> acc ^ ", " ^ stringifier elem)
+          (stringifier h) t ^ "]"
+
+
+(* Structured string *)
+
+let structured_string_of_hint hint =
+  let identity x = x in
+  sprintf "{hintid : \"%s\"; hintexp : %s}"
+    (hint.hintid.it)
+    (structured_string_of_list identity hint.hintexp)
+
+let structured_string_of_hints hints =
+    structured_string_of_list structured_string_of_hint hints
+
+let structured_string_of_atom = function
+  | Atom atomid -> sprintf "Atom \"%s\"" atomid
+  | Bot -> "Bot"
+  | Dot -> "Dot"
+  | Dot2 -> "Dot2"
+  | Dot3 -> "Dot3"
+  | Semicolon -> "Semicolon"
+  | Arrow -> "Arrow"
+  | Colon -> "Colon"
+  | Sub -> "Sub"
+  | SqArrow -> "SqArrow"
+  | Tilesturn -> "Tilesturn"
+  | Turnstile -> "Turnstile"
+  | LParen -> "LParen"
+  | LBrack -> "LBrack"
+  | LBrace -> "LBrace"
+  | RParen -> "RParen"
+  | RBrack -> "RBrack"
+  | RBrace -> "RBrace"
+  | Quest -> "Quest"
+  | Star -> "Star"
+
+let structured_string_of_unop = function
+  | NotOp -> "NotOp"
+  | PlusOp -> "PlusOp"
+  | MinusOp -> "MinusOp"
+
+let structured_string_of_binop = function
+  | AndOp -> "AndOp"
+  | OrOp -> "OrOp"
+  | ImplOp -> "ImplOp"
+  | EquivOp -> "EquivOp"
+  | AddOp -> "AddOp"
+  | SubOp -> "SubOp"
+  | MulOp -> "MulOp"
+  | DivOp -> "DivOp"
+  | ExpOp -> "ExpOp"
+
+let structured_string_of_cmpop = function
+  | EqOp -> "EqOp"
+  | NeOp -> "NeOp"
+  | LtOp -> "LtOp"
+  | GtOp -> "GtOp"
+  | LeOp -> "LeOp"
+  | GeOp -> "GeOp"
+
+let structured_string_of_mixop mixop =
+  structured_string_of_list
+    (structured_string_of_list structured_string_of_atom)
+    mixop
+
+
+(* Types *)
+
+let rec structured_string_of_iter iter =
+  match iter with
+  | Opt -> "Opt"
+  | List -> "List"
+  | List1 -> "List1"
+  | ListN exp -> sprintf "ListN (%s)" (structured_string_of_exp exp)
+
+and structured_string_of_typ typ =
+  match typ.it with
+  | VarT id -> sprintf "VarT \"%s\"" id.it
+  | BoolT -> "BoolT"
+  | NatT -> "NatT"
+  | TextT -> "TextT"
+  | TupT typs -> sprintf "TupT (%s)" (structured_string_of_typs typs)
+  | IterT (typ1, iter) ->
+      sprintf "IterT (%s, %s)"
+        (structured_string_of_typ typ1)
+        (structured_string_of_iter iter)
+
+and structured_string_of_typs typs =
+  structured_string_of_list structured_string_of_typ typs
+
+and structured_string_of_deftyp deftyp =
+  match deftyp.it with
+  | AliasT typ -> sprintf "AliasT (%s)" (structured_string_of_typ typ)
+  | NotationT (mixop, typ) ->
+      sprintf "NotationT (%s, %s)"
+        (structured_string_of_mixop mixop)
+        (structured_string_of_typ typ)
+  | StructT typfields ->
+      sprintf "StructT (%s)" (structured_string_of_typfields typfields)
+  | VariantT (typcases) ->
+      sprintf "VariantT (%s)"
+        (structured_string_of_list structured_string_of_typcase typcases)
+
+and structured_string_of_typfield (atom, typ, hints) =
+  sprintf "(%s, %s, %s)"
+    (structured_string_of_atom atom)
+    (structured_string_of_typ typ)
+    (structured_string_of_hints hints)
+
+and structured_string_of_typfields typfields =
+  structured_string_of_list structured_string_of_typfield typfields
+
+and structured_string_of_typcase (atom, typ, hints) =
+  sprintf "(%s, %s, %s)"
+    (structured_string_of_atom atom)
+    (structured_string_of_typ typ)
+    (structured_string_of_hints hints)
+
+
+(* Expressions *)
+
+and structured_string_of_exp exp =
+  match exp.it with
+  | VarE id -> sprintf "VarE \"%s\"" id.it
+  | BoolE b -> sprintf "BoolE %s" (string_of_bool b)
+  | NatE n -> sprintf "NatE %s" (string_of_int n)
+  | TextE t -> sprintf "TextE \"%s\"" (String.escaped t)
+  | UnE (unop, exp2) ->
+      sprintf "UnE (%s, %s)"
+        (structured_string_of_unop unop)
+        (structured_string_of_exp exp2)
+  | BinE (binop, exp1, exp2) ->
+      sprintf "BinE (%s, %s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_binop binop)
+        (structured_string_of_exp exp2)
+  | CmpE (cmpop, exp1, exp2) ->
+      sprintf "CmpE (%s, %s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_cmpop cmpop)
+        (structured_string_of_exp exp2)
+  | IdxE (exp1, exp2) ->
+      sprintf "IdxE (%s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_exp exp2)
+  | SliceE (exp1, exp2, exp3) ->
+      sprintf "SliceE (%s, %s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_exp exp2)
+        (structured_string_of_exp exp3)
+  | UpdE (exp1, path, exp2) ->
+      sprintf "UpdE (%s, %s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_path path)
+        (structured_string_of_exp exp2)
+  | ExtE (exp1, path, exp2) ->
+      sprintf "ExtE (%s, %s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_path path)
+        (structured_string_of_exp exp2)
+  | StrE expfields ->
+      sprintf "StrE (%s)"
+        (structured_string_of_list structured_string_of_expfield expfields)
+  | DotE (typ, exp1, atom) ->
+      sprintf "DotE (%s, %s, %s)"
+        (structured_string_of_typ typ)
+        (structured_string_of_exp exp1)
+        (structured_string_of_atom atom)
+  | CompE (exp1, exp2) ->
+      sprintf "CompE (%s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_exp exp2)
+  | LenE exp1 -> sprintf "LenE (%s)" (structured_string_of_exp exp1)
+  | TupE exps -> sprintf "TupE (%s)" (structured_string_of_exps exps)
+  | MixE (mixop, exp1) ->
+      sprintf "MixE (%s, %s)"
+        (structured_string_of_mixop mixop)
+        (structured_string_of_exp exp1)
+  | CallE (id, exp) ->
+      sprintf "CallE (\"%s\", %s)"
+        id.it
+        (structured_string_of_exp exp)
+  | IterE (exp1, iterexp) ->
+      sprintf "IterE (%s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_iterexp iterexp)
+  | OptE None -> "OptE ()"
+  | OptE Some exp -> sprintf "OptE (%s)" (structured_string_of_exp exp)
+  | TheE exp -> sprintf "TheE (%s)" (structured_string_of_exp exp)
+  | ListE exps -> sprintf "ListE (%s)" (structured_string_of_exps exps)
+  | CatE (exp1, exp2) ->
+      sprintf "CatE (%s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_exp exp2)
+  | CaseE (atom, exp1, typ) ->
+      sprintf "CaseE (%s, %s, %s)"
+        (structured_string_of_atom atom)
+        (structured_string_of_typ typ)
+        (structured_string_of_exp exp1)
+  | SubE (exp1, typ1, typ2) ->
+      sprintf "SubE (%s, %s, %s)"
+        (structured_string_of_exp exp1)
+        (structured_string_of_typ typ1)
+        (structured_string_of_typ typ2)
+
+and structured_string_of_exps exps =
+  structured_string_of_list structured_string_of_exp exps
+
+and structured_string_of_iterexp (iter, ids) =
+  sprintf "(%s, %s)"
+    (structured_string_of_iter iter)
+    (structured_string_of_list (fun id -> sprintf "\"%s\"" id.it) ids)
+
+
+and structured_string_of_expfield (atom, exp) =
+  sprintf "(%s, %s)" (structured_string_of_atom atom) (structured_string_of_exp exp)
+
+and structured_string_of_path path =
+  match path.it with
+  | RootP -> "RootP"
+  | IdxP (path1, exp) ->
+      sprintf "IdxP (%s, %s)"
+        (structured_string_of_path path1)
+        (structured_string_of_exp exp)
+  | DotP (path1, atom) ->
+      sprintf "DotP (%s, %s)"
+        (structured_string_of_path path1)
+        (structured_string_of_atom atom)
+
+
+(* Definitions *)
+
+let structured_string_of_bind (id, typ, iters) =
+  sprintf "(\"%s\", %s, %s)"
+    id.it
+    (structured_string_of_typ typ)
+    (structured_string_of_list structured_string_of_iter iters)
+
+let structured_string_of_binds binds =
+  structured_string_of_list structured_string_of_bind binds
+
+
+let rec structured_string_of_premise prem =
+  match prem.it with
+  | RulePr (id, mixop, exp) ->
+      sprintf "RulePr (\"%s\", %s, %s)"
+        id.it
+        (structured_string_of_mixop mixop)
+        (structured_string_of_exp exp)
+  | IfPr (exp) -> sprintf "IfPr (%s)" (structured_string_of_exp exp)
+  | ElsePr -> "ElsePr"
+  | IterPr (prem, iterexp) ->
+      sprintf "IterPr (%s, %s)"
+        (structured_string_of_premise prem)
+        (structured_string_of_iterexp iterexp)
+
+let structured_string_of_rule rule =
+  match rule.it with
+  | RuleD (id, binds, mixop, exp, prems) ->
+      sprintf "RuleD (\"%s\", %s, %s, %s, %s)"
+        id.it
+        (structured_string_of_binds binds)
+        (structured_string_of_mixop mixop)
+        (structured_string_of_exp exp)
+        (structured_string_of_list structured_string_of_premise prems)
+
+let structured_string_of_clause clause =
+  match clause.it with
+  | DefD (binds, exp1, exp2, prems) ->
+      sprintf "DefD (%s, %s, %s, %s)"
+        (structured_string_of_binds binds)
+        (structured_string_of_exp exp1)
+        (structured_string_of_exp exp2)
+        (structured_string_of_list structured_string_of_premise prems)
+
+let rec structured_string_of_def def =
+  match def.it with
+  | SynD (id, deftyp, hints) ->
+      sprintf "SynD (\n   \"%s\",\n   %s,\n   %s\n)"
+        id.it
+        (structured_string_of_deftyp deftyp)
+        (structured_string_of_hints hints)
+  | RelD (id, mixop, typ, rules, hints) ->
+      sprintf "RelD (\n   \"%s\",\n   %s,\n   %s,\n   %s,\n   %s\n)"
+        id.it
+        (structured_string_of_mixop mixop)
+        (structured_string_of_typ typ)
+        (structured_string_of_list structured_string_of_rule rules)
+        (structured_string_of_hints hints)
+  | DecD (id, typ1, typ2, clauses, hints) ->
+      sprintf "DecD (\n   \"%s\",\n   %s,\n   %s,\n   %s,\n   %s\n)"
+        id.it
+        (structured_string_of_typ typ1)
+        (structured_string_of_typ typ2)
+        (structured_string_of_list structured_string_of_clause clauses)
+        (structured_string_of_hints hints)
+  | RecD defs ->
+      sprintf "RecD (%s)" (structured_string_of_list structured_string_of_def defs)
+
+
+(* Scripts *)
+
+let structured_string_of_script defs =
+  concat "" (List.map (suffix structured_string_of_def "\n") defs)
