de4c668645648d7bef0c1563bcc3df0e5c9e092d
Andreas Rossberg <rossberg@mpi-sws.org>
2023-04-26 12:54:31 +0200

    ...

--- spectec/src/exe-watsup/main.ml
+++ spectec/src/exe-watsup/main.ml
@@ -78,7 +78,6 @@ let log s = if !log then Printf.printf "== %s\n%!" s
 let () =
   Printexc.record_backtrace true;
   try
-    log "Starting."
     Arg.parse argspec add_arg usage;
     log "Parsing...";
     let el = List.concat_map Frontend.Parse.parse_file !srcs in
