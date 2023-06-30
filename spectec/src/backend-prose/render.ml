79b3c4eeffcbdf4a8d562498627b0cb31b6fb79e
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2023-06-30 15:45:56 +0900

    Render al instructions

--- spectec/src/backend-prose/render.ml
+++ spectec/src/backend-prose/render.ml
@@ -1,15 +1,43 @@
 open Prose
+open Printf
 
-(* Environment *)
+(* Helpers *)
 
-module Set = Set.Make(String)
-module Map = Map.Make(String)
+let indent = "  "
+
+let render_opt prefix stringifier suffix = function
+  | None -> ""
+  | Some x -> prefix ^ stringifier x ^ suffix
+
+let rec repeat str num =
+  if num = 0 then ""
+  else if Int.rem num 2 = 0 then repeat (str ^ str) (num / 2)
+  else str ^ repeat (str ^ str) (num / 2)
+
+let render_order index depth =
+  index := !index + 1;
+
+  let num_idx = string_of_int !index in
+  let alp_idx = Char.escaped (Char.chr (96 + !index)) in
+
+  match depth mod 4 with
+  | 0 -> num_idx ^ "."
+  | 1 -> alp_idx ^ "."
+  | 2 -> num_idx ^ ")"
+  | 3 -> alp_idx ^ ")"
+  | _ -> failwith "unreachable" 
 
 (* Operators *)
 
+(* Expressions and Paths *)
+
+let render_expr expr = "TODO" ^ "(" ^ Al.Print.string_of_expr expr ^ ")"
+
+and render_path path = "TODO" ^ "(" ^ Al.Print.string_of_path path ^ ")"
+
 (* Conditions *)
 
-(* Expressions *)
+let render_cond cond = "TODO" ^ "(" ^ Al.Print.string_of_cond cond ^ ")"
 
 (* Instructions *)
 
@@ -26,24 +54,122 @@ let rec render_prose_instrs index depth = function
     | EquivI (_c1, _c2) -> "TODO" ^ render_prose_instrs (index + 1) depth is
     | YetI s -> s
 
-let rec render_al_instrs index depth = function
-  | [] -> ""
-  | i :: is -> 
-      (Al.Print.string_of_instr (ref index) depth i) ^ "\n" ^
-      render_al_instrs (index + 1) depth is 
+let rec render_al_instr index depth = function
+  | Al.Ast.IfI (c, il, []) ->
+      sprintf "%s If %s, then:%s" (render_order index depth) (render_cond c)
+        (render_al_instrs (depth + 1) il)
+  | Al.Ast.IfI (c, il1, [ IfI (inner_c, inner_il1, []) ]) ->
+      let if_index = render_order index depth in
+      let else_if_index = render_order index depth in
+      sprintf "%s If %s, then:%s\n%s Else if %s, then:%s"
+        if_index
+        (render_cond c)
+        (render_al_instrs (depth + 1) il1)
+        (repeat indent depth ^ else_if_index)
+        (render_cond inner_c)
+        (render_al_instrs (depth + 1) inner_il1)
+  | Al.Ast.IfI (c, il1, [ IfI (inner_c, inner_il1, inner_il2) ]) ->
+      let if_index = render_order index depth in
+      let else_if_index = render_order index depth in
+      let else_index = render_order index depth in
+      sprintf "%s If %s, then:%s\n%s Else if %s, then:%s\n%s Else:%s"
+        if_index
+        (render_cond c)
+        (render_al_instrs (depth + 1) il1)
+        (repeat indent depth ^ else_if_index)
+        (render_cond inner_c)
+        (render_al_instrs (depth + 1) inner_il1)
+        (repeat indent depth ^ else_index)
+        (render_al_instrs (depth + 1) inner_il2)
+  | Al.Ast.IfI (c, il1, il2) ->
+      let if_index = render_order index depth in
+      let else_index = render_order index depth in
+      sprintf "%s If %s, then:%s\n%s Else:%s" if_index (render_cond c)
+        (render_al_instrs (depth + 1) il1)
+        (repeat indent depth ^ else_index)
+        (render_al_instrs (depth + 1) il2)
+  | Al.Ast.OtherwiseI il ->
+      sprintf "%s Otherwise:%s" (render_order index depth)
+        (render_al_instrs (depth + 1) il)
+  | Al.Ast.WhileI (c, il) ->
+      sprintf "%s While %s, do:%s" (render_order index depth) (render_cond c)
+        (render_al_instrs (depth + 1) il)
+  | Al.Ast.EitherI (il1, il2) ->
+      let either_index = render_order index depth in
+      let or_index = render_order index depth in
+      sprintf "%s Either:%s\n%s Or:%s" either_index
+        (render_al_instrs (depth + 1) il1)
+        (repeat indent depth ^ or_index)
+        (render_al_instrs (depth + 1) il2)
+  | Al.Ast.ForI (e, il) ->
+      sprintf "%s For i in range |%s|:%s" (render_order index depth)
+        (render_expr e)
+        (render_al_instrs (depth + 1) il)
+  | Al.Ast.ForeachI (e1, e2, il) ->
+      sprintf "%s Foreach %s in %s:%s" (render_order index depth)
+        (render_expr e1)
+        (render_expr e2)
+        (render_al_instrs (depth + 1) il)
+  | Al.Ast.AssertI s -> sprintf "%s Assert: %s." (render_order index depth) s
+  | Al.Ast.PushI e ->
+      sprintf "%s Push %s to the stack." (render_order index depth)
+        (render_expr e)
+  | Al.Ast.PopI e ->
+      sprintf "%s Pop %s from the stack." (render_order index depth)
+        (render_expr e)
+  | Al.Ast.PopAllI e ->
+      sprintf "%s Pop all values %s from the stack." (render_order index depth)
+        (render_expr e)
+  | Al.Ast.LetI (n, e) ->
+      sprintf "%s Let %s be %s." (render_order index depth) (render_expr n)
+        (render_expr e)
+  | Al.Ast.TrapI -> sprintf "%s Trap." (render_order index depth)
+  | Al.Ast.NopI -> sprintf "%s Do nothing." (render_order index depth)
+  | Al.Ast.ReturnI e_opt ->
+      sprintf "%s Return%s." (render_order index depth)
+        (render_opt " " render_expr "" e_opt)
+  | Al.Ast.EnterI (e1, e2) ->
+      sprintf "%s Enter %s with label %s." (render_order index depth)
+        (render_expr e1) (render_expr e2)
+  | Al.Ast.ExecuteI e ->
+      sprintf "%s Execute %s." (render_order index depth) (render_expr e)
+  | Al.Ast.ExecuteSeqI e ->
+      sprintf "%s Execute the sequence (%s)." (render_order index depth) (render_expr e)
+  | Al.Ast.JumpI e ->
+      sprintf "%s Jump to %s." (render_order index depth) (render_expr e)
+  | Al.Ast.PerformI e ->
+      sprintf "%s Perform %s." (render_order index depth) (render_expr e)
+  | Al.Ast.ExitNormalI _ | Al.Ast.ExitAbruptI _ -> render_order index depth ^ " Exit current context."
+  | Al.Ast.ReplaceI (e1, p, e2) ->
+      sprintf "%s Replace %s%s with %s." (render_order index depth)
+        (render_expr e1) (render_path p) (render_expr e2)
+  | Al.Ast.AppendI (e1, e2) ->
+      sprintf "%s Append %s to the %s." (render_order index depth)
+        (render_expr e2) (render_expr e1)
+  | Al.Ast.AppendListI (e1, e2) ->
+      sprintf "%s Append the sequence %s to the %s." (render_order index depth)
+        (render_expr e2) (render_expr e1)
+  | Al.Ast.YetI s -> sprintf "%s YetI: %s." (render_order index depth) s
+
+and render_al_instrs depth instrs =
+  let index = ref 0 in
+  List.fold_left
+    (fun acc i ->
+      acc ^ "\n" ^ repeat indent depth ^ render_al_instr index depth i)
+    "" instrs
 
 (* Params *)
 
 let rec render_params = function
   | [] -> ""
   | p :: ps -> 
-    Al.Print.string_of_expr p ^ " " ^ render_params ps
+    render_expr p ^ " " ^ render_params ps
 
 (* Prose *)
 
 let render_title name params =
   ":math:`\\" ^ (String.uppercase_ascii name) ^ "~" ^
-  (List.fold_left (fun pstring p -> pstring ^ "\\" ^ Al.Print.string_of_expr p ^ "~") String.empty params) ^
+  (List.fold_left (fun pstring p -> pstring ^ "\\" ^ render_expr p ^ "~") String.empty params) ^
   "\\END`"
 
 let render_pred name params instrs = 
@@ -70,7 +196,7 @@ let render_algo name params instrs =
   let title = render_title name (List.map (fun p -> let (e, _) = p in e) params) in
   title ^ "\n" ^
   String.make (String.length title) '.' ^ "\n" ^
-  render_al_instrs 0 0 instrs
+  render_al_instrs 0 instrs
 
 let rec render_defs = function
   | [] -> ""
