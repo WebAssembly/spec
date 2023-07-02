7e70a90cfa1b0c04450c93999a3667dd198ae0d0
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2023-07-02 16:28:50 +0900

    Render inline math blocks

--- spectec/src/backend-prose/render.ml
+++ spectec/src/backend-prose/render.ml
@@ -5,14 +5,18 @@ open Printf
 
 let indent = "  "
 
-let render_opt prefix stringifier suffix = function
-  | None -> ""
-  | Some x -> prefix ^ stringifier x ^ suffix
-
 let rec repeat str num =
   if num = 0 then ""
   else if Int.rem num 2 = 0 then repeat (str ^ str) (num / 2)
   else str ^ repeat (str ^ str) (num / 2)
+
+let math = ":math:"
+
+let render_math s = math ^ sprintf "`%s`" s
+
+let render_opt prefix stringifier suffix = function
+  | None -> ""
+  | Some x -> prefix ^ stringifier x ^ suffix
 
 let render_order index depth =
   index := !index + 1;
@@ -73,12 +77,16 @@ let render_al_mathop = function
 (* Names and Iters *)
 
 let rec render_name = function
-  | Al.Ast.N s -> s
+  | Al.Ast.N s -> 
+      if String.length s > 1 then
+        sprintf "\\%s" s 
+      else
+        s
   | Al.Ast.SubN (n, s) -> sprintf "%s_%s" (render_name n) s
 
 let render_iter = function
-  | Al.Ast.Opt -> "?"
-  | Al.Ast.List -> "*"
+  | Al.Ast.Opt -> "^?"
+  | Al.Ast.List -> "^\\ast"
   | Al.Ast.List1 -> "+"
   | Al.Ast.ListN name -> "^" ^ render_name name
 
@@ -86,85 +94,115 @@ let render_iters iters = List.map render_iter iters |> List.fold_left (^) ""
 
 (* Expressions and Paths *)
 
-let rec render_expr = function
-  | Al.Ast.NumE i -> Int64.to_string i
+let rec render_expr in_math = function
+  | Al.Ast.NumE i -> 
+      let si = Int64.to_string i in
+      if in_math then si else render_math si
   | Al.Ast.StringE s -> s
-  | Al.Ast.MinusE e -> sprintf "-%s" (render_expr e)
+  | Al.Ast.MinusE e -> 
+      let se = render_expr in_math e in
+      let s = sprintf "-%s" se in
+      if in_math then s else render_math s
   | Al.Ast.BinopE (op, e1, e2) ->
-      sprintf "(%s %s %s)" (render_expr e1) (render_al_mathop op) (render_expr e2)
-  | Al.Ast.PairE (e1, e2) -> sprintf "(%s, %s)" (render_expr e1) (render_expr e2)
-  | Al.Ast.AppE (n, el) ->
-      sprintf "$%s(%s)" (render_name n)
-        (render_list render_expr "" ", " "" el)
+      let sop = render_al_mathop op in
+      let se1 = render_expr true e1 in
+      let se2 = render_expr true e2 in
+      let s = sprintf "%s%s%s" se1 sop se2 in
+      if in_math then s else render_math s
+  | Al.Ast.PairE (e1, e2) -> sprintf "(%s, %s)" (render_expr in_math e1) (render_expr in_math e2)
+  | Al.Ast.AppE (n, es) ->
+      let sn = render_name n in
+      let ses = render_list (render_expr true) "" ", " "" es in
+      let s = sprintf "%s(%s)" sn ses in
+      if in_math then s else render_math s
   | Al.Ast.MapE (n, el, iters) ->
       sprintf "$%s(%s)%s" (render_name n)
-        (render_list render_expr "" ", " "" el)
+        (render_list (render_expr in_math) "" ", " "" el)
         (render_iters iters)
   | Al.Ast.ConcatE (e1, e2) ->
-      sprintf "%s ++ %s" (render_expr e1) (render_expr e2)
-  | Al.Ast.LengthE e -> sprintf "|%s|" (render_expr e)
-  | Al.Ast.ArityE e -> sprintf "the arity of %s" (render_expr e)
+      sprintf "%s ++ %s" (render_expr in_math e1) (render_expr in_math e2)
+  | Al.Ast.LengthE e -> 
+      let se = render_expr true e in
+      "the length of " ^
+      if in_math then se else render_math se
+  | Al.Ast.ArityE e -> sprintf "the arity of %s" (render_expr in_math e)
   | Al.Ast.GetCurLabelE -> "the current label"
   | Al.Ast.GetCurFrameE -> "the current frame"
   | Al.Ast.GetCurContextE -> "the current context"
   | Al.Ast.FrameE (e1, e2) ->
-      sprintf "the activation of %s with arity %s" (render_expr e2)
-        (render_expr e1)
-  | Al.Ast.ListE el -> render_list render_expr "[" ", " "]" el
-  | Al.Ast.ListFillE (e1, e2) -> render_expr e1 ^ "^" ^ render_expr e2
-  | Al.Ast.AccessE (e, p) -> sprintf "%s%s" (render_expr e) (render_path p)
+      sprintf "the activation of %s with arity %s" (render_expr in_math e2)
+        (render_expr in_math e1)
+  | Al.Ast.ListE el -> render_list (render_expr in_math) "[" ", " "]" el
+  | Al.Ast.ListFillE (e1, e2) -> render_expr in_math e1 ^ "^" ^ render_expr in_math e2
+  | Al.Ast.AccessE (e, p) -> sprintf "%s%s" (render_expr in_math e) (render_path in_math p)
   | Al.Ast.ExtendE (e1, ps, e2) ->
-      sprintf "%s with %s extended by %s" (render_expr e1) (render_paths ps) (render_expr e2)
+      sprintf "%s with %s extended by %s" (render_expr in_math e1) (render_paths in_math ps) (render_expr in_math e2)
   | Al.Ast.ReplaceE (e1, ps, e2) ->
-      sprintf "%s with %s replaced by %s" (render_expr e1) (render_paths ps) (render_expr e2)
+      sprintf "%s with %s replaced by %s" (render_expr in_math e1) (render_paths in_math ps) (render_expr in_math e2)
   | Al.Ast.RecordE r ->
       Al.Record.Record.fold
-        (fun k v acc -> acc ^ k ^ ": " ^ render_expr v ^ "; ")
+        (fun k v acc -> acc ^ k ^ ": " ^ render_expr in_math v ^ "; ")
         r "{ "
       ^ "}"
-  | Al.Ast.ContE e -> sprintf "the continuation of %s" (render_expr e)
+  | Al.Ast.ContE e -> sprintf "the continuation of %s" (render_expr in_math e)
   | Al.Ast.LabelE (e1, e2) ->
-      sprintf "the label_%s{%s}" (render_expr e1) (render_expr e2)
-  | Al.Ast.NameE n -> render_name n
-  | Al.Ast.IterE (e, iter) -> render_expr e ^ render_iter iter
+      sprintf "the label whose arity is %s and whose continuation is %s" (render_expr in_math e1) (render_expr in_math e2)
+  | Al.Ast.NameE n -> 
+      let sn = render_name n in
+      if in_math then sn else render_math sn 
+  | Al.Ast.IterE (Al.Ast.NameE n, iter) -> 
+      let sn = render_name n in
+      let siter = render_iter iter in
+      let s = sprintf "%s%s" sn siter in
+      if in_math then s else render_math s
+  | Al.Ast.IterE (e, iter) -> render_expr in_math e ^ render_iter iter
   | Al.Ast.ArrowE (e1, e2) ->
-    (match e1 with ListE _ -> render_expr e1 | _ -> "[" ^ render_expr e1 ^ "]" )
-    ^ "->"
-    ^ (match e2 with ListE _ -> render_expr e2 | _ -> "[" ^ render_expr e2 ^ "]" )
-  | Al.Ast.ConstructE ("CONST", hd::tl) -> "(" ^ render_expr hd ^ ".CONST" ^ render_list render_expr " " " " "" tl ^ ")"
-  | Al.Ast.ConstructE (s, []) -> s
-  | Al.Ast.ConstructE (s, el) -> "(" ^ s ^ render_list render_expr " " " " "" el ^ ")"
-  | Al.Ast.OptE (Some e) -> "?(" ^ render_expr e ^ ")"
+      let se1 = (match e1 with ListE _ -> render_expr true e1 | _ -> "[" ^ render_expr true e1 ^ "]" ) in
+      let se2 = (match e2 with ListE _ -> render_expr true e2 | _ -> "[" ^ render_expr true e2 ^ "]" ) in
+      let s = sprintf "%s \\to %s" se1 se2 in
+      if in_math then s else render_math s
+  | Al.Ast.ConstructE (s, []) ->
+      let s = sprintf "\\%s" (String.uppercase_ascii s) in
+      if in_math then s else render_math s
+  | Al.Ast.ConstructE (s, es) -> 
+      let s = String.uppercase_ascii s in
+      let ses = render_list (render_expr true) "" "~" "" es in
+      let s = 
+        sprintf "\\%s~%s" s ses ^ 
+        if List.length es > 1 then "~\\END" else ""
+      in
+      if in_math then s else render_math s
+  | Al.Ast.OptE (Some e) -> "?(" ^ render_expr in_math e ^ ")"
   | Al.Ast.OptE None -> "?()"
   | Al.Ast.YetE s -> sprintf "YetE (%s)" s
 
-and render_path = function 
-  | Al.Ast.IndexP e -> sprintf "[%s]" (render_expr e)
+and render_path in_math = function 
+  | Al.Ast.IndexP e -> sprintf "[%s]" (render_expr in_math e)
   | Al.Ast.SliceP (e1, e2) ->
-      sprintf "[%s : %s]" (render_expr e1) (render_expr e2)
+      sprintf "[%s : %s]" (render_expr in_math e1) (render_expr in_math e2)
   | Al.Ast.DotP s -> sprintf ".%s" s
 
-and render_paths paths = List.map render_path paths |> List.fold_left (^) ""
+and render_paths in_math paths = List.map (render_path in_math) paths |> List.fold_left (^) ""
 
 (* Conditions *)
 
-and render_cond = function 
+and render_cond in_math = function 
   | Al.Ast.NotC (Al.Ast.IsCaseOfC (e, c)) ->
-      sprintf "%s is not of the case %s" (render_expr e) c
+      sprintf "%s is not of the case %s" (render_expr in_math e) c
   | Al.Ast.NotC (Al.Ast.IsDefinedC e) ->
-      sprintf "%s is not defined" (render_expr e)
+      sprintf "%s is not defined" (render_expr in_math e)
   | Al.Ast.NotC (Al.Ast.ValidC e) ->
-      sprintf "%s is not valid" (render_expr e)
-  | Al.Ast.NotC c -> sprintf "not %s" (render_cond c)
+      sprintf "%s is not valid" (render_expr in_math e)
+  | Al.Ast.NotC c -> sprintf "not %s" (render_cond in_math c)
   | Al.Ast.BinopC (op, c1, c2) ->
-      sprintf "%s %s %s" (render_cond c1) (render_al_logop op) (render_cond c2)
+      sprintf "%s %s %s" (render_cond in_math c1) (render_al_logop op) (render_cond in_math c2)
   | Al.Ast.CompareC (op, e1, e2) ->
-      sprintf "%s %s %s" (render_expr e1) (render_al_cmpop op) (render_expr e2)
-  | Al.Ast.ContextKindC (s, e) -> sprintf "%s is %s" (render_expr e) s
-  | Al.Ast.IsDefinedC e -> sprintf "%s is defined" (render_expr e)
-  | Al.Ast.IsCaseOfC (e, c) -> sprintf "%s is of the case %s" (render_expr e) c
+      sprintf "%s %s %s" (render_expr in_math e1) (render_al_cmpop op) (render_expr in_math e2)
+  | Al.Ast.ContextKindC (s, e) -> sprintf "%s is %s" (render_expr in_math e) s
+  | Al.Ast.IsDefinedC e -> sprintf "%s is defined" (render_expr in_math e)
+  | Al.Ast.IsCaseOfC (e, c) -> sprintf "%s is of the case %s" (render_expr in_math e) c
   | Al.Ast.IsTopC s -> sprintf "the top of the stack is %s" s
-  | Al.Ast.ValidC e -> sprintf "%s is valid" (render_expr e)
+  | Al.Ast.ValidC e -> sprintf "%s is valid" (render_expr in_math e)
   | Al.Ast.YetC s -> sprintf "YetC (%s)" s
 
 (* Instructions *)
@@ -172,31 +210,31 @@ and render_cond = function
 let rec render_prose_instr depth = function
   | LetI (e1, e2) ->
       sprintf "* Let %s be %s."
-        (render_expr e1)
-        (render_expr e2)
+        (render_expr false e1)
+        (render_expr false e2)
   | CmpI (e1, cmpop, e2) ->
       sprintf "* %s must be %s %s."
-        (render_expr e1)
+        (render_expr false e1)
         (render_prose_cmpop cmpop)
-        (render_expr e2)
+        (render_expr false e2)
   | MustValidI (e1, e2, e3) -> 
       sprintf "* Under the context %s, %s must be valid%s."
-        (render_expr e1)
-        (render_expr e2)
-        (render_opt " with type " render_expr "" e3)
+        (render_expr false e1)
+        (render_expr false e2)
+        (render_opt " with type " (render_expr false) "" e3)
   | MustMatchI (e1, e2) ->
       sprintf "* %s must match %s."
-        (render_expr e1)
-        (render_expr e2)
+        (render_expr false e1)
+        (render_expr false e2)
   | IsValidI e ->
       sprintf "* The instruction is valid%s."
-        (render_opt " with type " render_expr "" e)
+        (render_opt " with type " (render_expr false) "" e)
   | ForallI (s, is) ->
       sprintf "* %s%s" s (render_prose_instrs (depth + 1) is)
   | EquivI (c1, c2) ->
       sprintf "* %s and %s are equivalent."
-        (render_cond c1)
-        (render_cond c2)
+        (render_cond false c1)
+        (render_cond false c2)
   | YetI s ->
       sprintf "* YetI: %s." s
 
@@ -208,17 +246,17 @@ and render_prose_instrs depth instrs =
 
 let rec render_al_instr index depth = function
   | Al.Ast.IfI (c, il, []) ->
-      sprintf "%s If %s, then:%s" (render_order index depth) (render_cond c)
+      sprintf "%s If %s, then:%s" (render_order index depth) (render_cond false c)
         (render_al_instrs (depth + 1) il)
   | Al.Ast.IfI (c, il1, [ IfI (inner_c, inner_il1, []) ]) ->
       let if_index = render_order index depth in
       let else_if_index = render_order index depth in
       sprintf "%s If %s, then:%s\n\n%s Else if %s, then:%s"
         if_index
-        (render_cond c)
+        (render_cond false c)
         (render_al_instrs (depth + 1) il1)
         (repeat indent depth ^ else_if_index)
-        (render_cond inner_c)
+        (render_cond false inner_c)
         (render_al_instrs (depth + 1) inner_il1)
   | Al.Ast.IfI (c, il1, [ IfI (inner_c, inner_il1, inner_il2) ]) ->
       let if_index = render_order index depth in
@@ -226,17 +264,17 @@ let rec render_al_instr index depth = function
       let else_index = render_order index depth in
       sprintf "%s If %s, then:%s\n\n%s Else if %s, then:%s\n%s Else:%s"
         if_index
-        (render_cond c)
+        (render_cond false c)
         (render_al_instrs (depth + 1) il1)
         (repeat indent depth ^ else_if_index)
-        (render_cond inner_c)
+        (render_cond false inner_c)
         (render_al_instrs (depth + 1) inner_il1)
         (repeat indent depth ^ else_index)
         (render_al_instrs (depth + 1) inner_il2)
   | Al.Ast.IfI (c, il1, il2) ->
       let if_index = render_order index depth in
       let else_index = render_order index depth in
-      sprintf "%s If %s, then:%s\n\n%s Else:%s" if_index (render_cond c)
+      sprintf "%s If %s, then:%s\n\n%s Else:%s" if_index (render_cond false c)
         (render_al_instrs (depth + 1) il1)
         (repeat indent depth ^ else_index)
         (render_al_instrs (depth + 1) il2)
@@ -244,7 +282,7 @@ let rec render_al_instr index depth = function
       sprintf "%s Otherwise:%s" (render_order index depth)
         (render_al_instrs (depth + 1) il)
   | Al.Ast.WhileI (c, il) ->
-      sprintf "%s While %s, do:%s" (render_order index depth) (render_cond c)
+      sprintf "%s While %s, do:%s" (render_order index depth) (render_cond false c)
         (render_al_instrs (depth + 1) il)
   | Al.Ast.EitherI (il1, il2) ->
       let either_index = render_order index depth in
@@ -255,60 +293,61 @@ let rec render_al_instr index depth = function
         (render_al_instrs (depth + 1) il2)
   | Al.Ast.ForI (e, il) ->
       sprintf "%s For i in range |%s|:%s" (render_order index depth)
-        (render_expr e)
+        (render_expr false e)
         (render_al_instrs (depth + 1) il)
   | Al.Ast.ForeachI (e1, e2, il) ->
       sprintf "%s Foreach %s in %s:%s" (render_order index depth)
-        (render_expr e1)
-        (render_expr e2)
+        (render_expr false e1)
+        (render_expr false e2)
         (render_al_instrs (depth + 1) il)
   | Al.Ast.AssertI s -> sprintf "%s Assert: %s." (render_order index depth) s
   | Al.Ast.PushI e ->
       sprintf "%s Push %s to the stack." (render_order index depth)
-        (render_expr e)
+        (render_expr false e)
   | Al.Ast.PopI e ->
       sprintf "%s Pop %s from the stack." (render_order index depth)
-        (render_expr e)
+        (render_expr false e)
   | Al.Ast.PopAllI e ->
       sprintf "%s Pop all values %s from the stack." (render_order index depth)
-        (render_expr e)
+        (render_expr false e)
   | Al.Ast.LetI (n, e) ->
-      sprintf "%s Let %s be %s." (render_order index depth) (render_expr n)
-        (render_expr e)
+      sprintf "%s Let %s be %s." (render_order index depth) (render_expr false n)
+        (render_expr false e)
   | Al.Ast.CallI (e, n, es) ->
       sprintf "%s Let %s be the result of computing %s." (render_order index depth)
-        (render_expr e)
-        (render_expr (Al.Ast.AppE(n, es)))
+        (render_expr false e)
+        (render_expr false (Al.Ast.AppE(n, es)))
   | Al.Ast.MapI (e, n, es, its) ->
       sprintf "%s Let %s be the result of computing %s." (render_order index depth)
-        (render_expr e)
-        (render_expr (Al.Ast.MapE(n, es, its)))
+        (render_expr false e)
+        (render_expr false (Al.Ast.MapE(n, es, its)))
   | Al.Ast.TrapI -> sprintf "%s Trap." (render_order index depth)
   | Al.Ast.NopI -> sprintf "%s Do nothing." (render_order index depth)
   | Al.Ast.ReturnI e_opt ->
       sprintf "%s Return%s." (render_order index depth)
-        (render_opt " " render_expr "" e_opt)
+        (render_opt " " (render_expr false) "" e_opt)
   | Al.Ast.EnterI (e1, e2) ->
       sprintf "%s Enter %s with label %s." (render_order index depth)
-        (render_expr e1) (render_expr e2)
+        (render_expr false e1) (render_expr false e2)
   | Al.Ast.ExecuteI e ->
-      sprintf "%s Execute %s." (render_order index depth) (render_expr e)
+      print_endline (Al.Print.structured_string_of_expr e);
+      sprintf "%s Execute %s." (render_order index depth) (render_expr false e)
   | Al.Ast.ExecuteSeqI e ->
-      sprintf "%s Execute the sequence (%s)." (render_order index depth) (render_expr e)
+      sprintf "%s Execute the sequence (%s)." (render_order index depth) (render_expr false e)
   | Al.Ast.JumpI e ->
-      sprintf "%s Jump to %s." (render_order index depth) (render_expr e)
+      sprintf "%s Jump to %s." (render_order index depth) (render_expr false e)
   | Al.Ast.PerformI (n, es) ->
-      sprintf "%s Perform %s." (render_order index depth) (render_expr (Al.Ast.AppE (n, es)))
+      sprintf "%s Perform %s." (render_order index depth) (render_expr false (Al.Ast.AppE (n, es)))
   | Al.Ast.ExitNormalI _ | Al.Ast.ExitAbruptI _ -> render_order index depth ^ " Exit current context."
   | Al.Ast.ReplaceI (e1, p, e2) ->
       sprintf "%s Replace %s%s with %s." (render_order index depth)
-        (render_expr e1) (render_path p) (render_expr e2)
+        (render_expr false e1) (render_path false p) (render_expr false e2)
   | Al.Ast.AppendI (e1, e2) ->
       sprintf "%s Append %s to the %s." (render_order index depth)
-        (render_expr e2) (render_expr e1)
+        (render_expr false e2) (render_expr false e1)
   | Al.Ast.AppendListI (e1, e2) ->
       sprintf "%s Append the sequence %s to the %s." (render_order index depth)
-        (render_expr e2) (render_expr e1)
+        (render_expr false e2) (render_expr false e1)
   | Al.Ast.YetI s -> sprintf "%s YetI: %s." (render_order index depth) s
 
 and render_al_instrs depth instrs =
@@ -320,17 +359,14 @@ and render_al_instrs depth instrs =
 
 (* Params *)
 
-let rec render_params = function
+let rec render_params in_math = function
   | [] -> ""
   | p :: ps -> 
-    render_expr p ^ " " ^ render_params ps
+    render_expr in_math p ^ " " ^ render_params in_math ps
 
 (* Prose *)
 
-let render_title name params =
-  ":math:`\\" ^ (String.uppercase_ascii name) ^ "~" ^
-  (List.fold_left (fun pstring p -> pstring ^ "\\" ^ render_expr p ^ "~") String.empty params) ^
-  "\\END`"
+let render_title name params = render_expr false (Al.Ast.ConstructE (name, params))
 
 let render_pred name params instrs = 
   let prefix = "validation_of_" in
