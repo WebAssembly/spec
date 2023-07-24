b2d7074408f8dddef96b0808ecefb9b2e17944bb
Wonho <new170527@kaist.ac.kr>
2023-07-24 18:17:05 +0900

    Refactor AL free

---
+++ spectec/src/al/free.ml
@@ -0,0 +1,37 @@
+open Ast
+
+let rec free_expr = function
+  | NumE _
+  | StringE _
+  | GetCurLabelE
+  | GetCurContextE
+  | GetCurFrameE
+  | YetE _ -> []
+  | NameE n -> [n]
+  | MinusE e
+  | LengthE e
+  | ArityE e
+  | ContE e -> free_expr e
+  | BinopE (_, e1, e2)
+  | ListFillE (e1, e2)
+  | ConcatE (e1, e2)
+  | PairE (e1, e2)
+  | ArrowE (e1, e2)
+  | FrameE (e1, e2)
+  | LabelE (e1, e2) -> free_expr e1 @ free_expr e2
+  | AppE (_, es)
+  | ListE es
+  | ConstructE (_, es) -> List.concat_map free_expr es
+  | RecordE _ -> (* TODO *) []
+  | AccessE (e, p) -> free_expr e @ free_path p
+  | ExtendE (e1, ps, e2, _)
+  | ReplaceE (e1, ps, e2) -> free_expr e1 @ List.concat_map free_path ps @ free_expr e2
+  | OptE e_opt -> List.concat_map free_expr (Option.to_list e_opt)
+  | IterE (e, _, i) -> free_expr e @ free_iter i
+and free_iter = function
+  | Opt
+  | List
+  | List1 -> []
+  | ListN n -> [n]
+  | IndexedListN (n, e) -> n :: free_expr e
+and free_path _ = [] (* TODO *)
--- spectec/src/backend-interpreter/translate.ml
+++ spectec/src/backend-interpreter/translate.ml
@@ -27,46 +27,6 @@ let list_partition_with pred xs =
 
 let disjoint xs ys = List.for_all (fun x -> List.for_all ((<>) x) ys) xs
 
-(* TODO: move these to al module *)
-let rec free_name = function
-  | N s -> [ s ]
-  | SubN (n, _) -> free_name n
-let rec free_expr = function
-  | NumE _
-  | StringE _
-  | GetCurLabelE
-  | GetCurContextE
-  | GetCurFrameE
-  | YetE _ -> []
-  | NameE n -> free_name n
-  | MinusE e
-  | LengthE e
-  | ArityE e
-  | ContE e -> free_expr e
-  | BinopE (_, e1, e2)
-  | ListFillE (e1, e2)
-  | ConcatE (e1, e2)
-  | PairE (e1, e2)
-  | ArrowE (e1, e2)
-  | FrameE (e1, e2)
-  | LabelE (e1, e2) -> free_expr e1 @ free_expr e2
-  | AppE (_, es)
-  | ListE es
-  | ConstructE (_, es) -> List.concat_map free_expr es
-  | RecordE _ -> (* TODO *) []
-  | AccessE (e, p) -> free_expr e @ free_path p
-  | ExtendE (e1, ps, e2, _)
-  | ReplaceE (e1, ps, e2) -> free_expr e1 @ List.concat_map free_path ps @ free_expr e2
-  | OptE e_opt -> List.concat_map free_expr (Option.to_list e_opt)
-  | IterE (e, _, i) -> free_expr e @ free_iter i
-and free_iter = function
-  | Opt
-  | List
-  | List1 -> []
-  | ListN n -> free_name n
-  | IndexedListN (n, e) -> free_name n @ free_expr e
-and free_path _ = [] (* TODO *)
-
 (* transform z; e into e *)
 let drop_state e = match e.it with
 | Ast.MixE (* z; e *)
@@ -514,7 +474,7 @@ let extract_bound_names lhs rhs targets cont = match lhs with
     new_lhs, new_rhs, cont
   | _ ->
     let contains_bound_name e =
-      let names = free_expr e in
+      let names = Al.Free.free_expr e in
       names > [] && disjoint names targets in
     let traverse e =
       let conds = ref [] in
@@ -677,7 +637,8 @@ let prems2instrs remain_lhs =
       | Ast.LetPr (exp1, exp2, targets) ->
           let instrs' = List.concat_map (bound_by exp1) remain_lhs @ instrs in
           init_lhs_id();
-          letI (exp2expr exp1) (exp2expr exp2) targets instrs'
+          let al_targets = List.map (fun s -> N s) targets in
+          letI (exp2expr exp1) (exp2expr exp2) al_targets instrs'
       | Ast.RulePr (id, _, exp) when String.ends_with ~suffix:"_ok" id.it ->
         ( match exp2args exp with
         | [ lim ] -> [ IfI (ValidC lim, instrs |> check_nop, []) ]
