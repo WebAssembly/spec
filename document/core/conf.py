c15f46c71695b155492577af54cbc45a770cad65
Andreas Rossberg <rossberg@mpi-sws.org>
2023-10-13 10:49:13 +0200

    [spec] Fix deps

--- document/core/conf.py
+++ document/core/conf.py
@@ -66,10 +66,10 @@ editor = u'Andreas Rossberg (editor)'
 logo = 'static/webassembly.png'
 
 # The name of the GitHub repository this resides in
-repo = 'tail-call'
+repo = 'spec'
 
 # The name of the proposal it represents, if any
-proposal = 'tail calls'
+proposal = ''
 
 # The draft version string (clear out for release cuts)
 draft = ' (Draft ' + date.today().strftime("%Y-%m-%d") + ')'
