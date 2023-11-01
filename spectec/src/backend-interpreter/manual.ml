4f8b88a095d68133fbe206df83411436e78f6724
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2023-11-01 16:28:45 +0900

    Add manual group_bytes_by helper function

--- spectec/src/backend-interpreter/manual.ml
+++ spectec/src/backend-interpreter/manual.ml
@@ -14,5 +14,34 @@ let exec_expr_const =
     ]
   )
 
+(* Helper for the manual array_new.data algorithm *)
+
+let group_bytes_by =
+  let n = NameE "n" in
+  let n' = NameE "n'" in
+
+  let bytes_ = IterE (NameE "byte", ["byte"], List) in
+  let bytes_left = ListE [AccessE (bytes_, SliceP (NumE 0L, n))] in
+  let bytes_right = AppE 
+    (
+      "group_bytes_by", 
+      [ n; AccessE (bytes_, SliceP (n, BinopE (Sub, n', n))) ]
+    ) 
+  in
+
+  FuncA (
+    "group_bytes_by",
+    [n; bytes_],
+    [
+      LetI (n', LengthE bytes_);
+      IfI (
+        CompareC (Ge, n', n),
+        [ ReturnI (Some (ConcatE (bytes_left, bytes_right))) ],
+        []
+      );
+      ReturnI (Some (ListE []));
+    ]
+  )
+
 let manual_algos =
-  [exec_expr_const] |> List.map Transpile.app_remover
+  [exec_expr_const; group_bytes_by] |> List.map Transpile.app_remover
