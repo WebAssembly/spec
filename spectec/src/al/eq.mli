53d6aece1f61607b62e858508c40f0c34a59c5c8
presenthee <khyunhii@gmail.com>
2024-01-08 15:29:33 +0900

    Add missing mli files under al

---
+++ spectec/src/al/eq.mli
@@ -0,0 +1,4 @@
+open Ast
+
+val eq_instr : instr -> instr -> bool
+val eq_instrs : instr list -> instr list -> bool
---
+++ spectec/src/al/free.mli
@@ -0,0 +1,9 @@
+open Ast 
+
+module IdSet : Set.S with type elt = string
+
+val intersection : id list -> id list -> id list
+
+val free_cond : cond -> id list
+val free_expr : expr -> id list
+val free_instr : instr -> id list
