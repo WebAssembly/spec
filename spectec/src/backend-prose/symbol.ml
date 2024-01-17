965f3a4bea1d3c52676881f136ef092237c64fd2
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2024-01-17 20:16:18 +0900

    Fix bug when extracting keywords from EL in AL renderer

--- spectec/src/backend-prose/symbol.ml
+++ spectec/src/backend-prose/symbol.ml
@@ -43,7 +43,7 @@ let rec extract_typ_kwds typ =
   | El.Ast.SeqT tl -> List.concat_map (fun t -> extract_typ_kwds t.it) tl
   | _ -> []
 
-let extract_syntax_kwds def =
+let extract_syntax_kwds' def =
   match def.it with
   | El.Ast.SynD (id, subid, _, typ, _) ->
       let topsyntax, syntax =
@@ -56,33 +56,45 @@ let extract_syntax_kwds def =
       Some (topsyntax, syntax, terminals, nonterminals)
   | _ -> None
 
-let extract_func_kwds def =
+let extract_syntax_kwds el =
+  List.fold_left
+    (fun acc def -> match extract_syntax_kwds' def with
+      | Some (topsyntax, syntax, terminals, nonterminals) ->
+          (* Update kwd mapping from syntax to terminals and nonterminals *)
+          let terminals', nonterminals' =
+            (match Map.find_opt syntax acc with
+            | Some (terminals', nonterminals') -> 
+                (Set.union terminals terminals', Set.union nonterminals nonterminals') 
+            | None -> (terminals, nonterminals))
+          in
+          let acc = Map.add syntax (terminals', nonterminals') acc in
+          (* Add level of indirection for subid-ed grammer *)
+          (match topsyntax with
+          | Some topsyntax ->
+              let terminals, nonterminals =
+                (match Map.find_opt topsyntax acc with
+                | Some (terminals, nonterminals) -> (terminals, Set.add syntax nonterminals)
+                | None -> (Set.empty, Set.singleton syntax))
+              in
+              Map.add topsyntax (terminals, nonterminals) acc
+          | None -> acc)
+      | _ -> acc)
+    Map.empty el
+
+let extract_func_kwds' def =
   match def.it with
   | El.Ast.DecD (id, _, _, _) -> [ id.it ]
   | _ -> []
 
+let extract_func_kwds el =
+  let funcs = List.concat_map extract_func_kwds' el in
+  List.fold_left (fun s acc -> Set.add acc s) Set.empty funcs
+
 (* Environment Construction *)
 
 let env el =
-  let kwds =
-    List.fold_left
-      (fun acc def -> match extract_syntax_kwds def with
-        | Some (topsyntax, syntax, terminals, nonterminals) ->
-            let acc = Map.add syntax (terminals, nonterminals) acc in
-            (match topsyntax with
-            | Some topsyntax ->
-                let terminals, nonterminals =
-                  (match Map.find_opt topsyntax acc with
-                  | Some (terminals, nonterminals) -> (terminals, Set.add syntax nonterminals)
-                  | None -> (Set.empty, Set.singleton syntax))
-                in
-                Map.add topsyntax (terminals, nonterminals) acc
-            | None -> acc)
-        | _ -> acc)
-      Map.empty el
-  in
-  let funcs = List.concat_map extract_func_kwds el in
-  let funcs = List.fold_left (fun s acc -> Set.add acc s) Set.empty funcs in
+  let kwds = extract_syntax_kwds el in
+  let funcs = extract_func_kwds el in
   { kwds = ref kwds; funcs = ref funcs; }
 
 (* Environment Lookup *)
