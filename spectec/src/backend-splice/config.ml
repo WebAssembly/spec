f92b2b57318827c168d89375892cd4cdd835184f
DJ <f52985@gmail.com>
2024-01-24 14:47:00 +0900

    Fix compile error

--- spectec/src/backend-splice/config.ml
+++ spectec/src/backend-splice/config.ml
@@ -31,8 +31,8 @@ let latex =
       {token = "@@"; prefix = "$"; suffix ="$"; indent = ""};
       {token = "@@@"; prefix = "$$\n"; suffix = "\n$$"; indent = ""};
     ];
-    latex = Backend_latex.Config.latex;
-    prose = Backend_prose.Config.latex;
+    latex = Backend_latex.Config.default;
+    prose = Backend_prose.Config.default;
   }
 
 let sphinx =
@@ -40,6 +40,6 @@ let sphinx =
       {token = "$"; prefix = ":math:`"; suffix ="`"; indent = ""};
       {token = "$$"; prefix = ".. math::\n   "; suffix = ""; indent = "   "};
     ];
-    latex = Backend_latex.Config.sphinx;
-    prose = Backend_prose.Config.sphinx;
+    latex = Backend_latex.Config.default;
+    prose = Backend_prose.Config.default;
   }
