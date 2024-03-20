6797cb5a82ed9842ed3dcbf0d5db2545a3c979ee
presenthee <khyunhii@gmail.com>
2024-03-20 09:52:09 +0900

    change an error message in il2il

--- spectec/src/il2al/il2il.ml
+++ spectec/src/il2al/il2il.ml
@@ -209,7 +209,7 @@ let rec collect_unified template e = if eq_exp template e then [], [] else
     | ListE es1, ListE es2 ->
       List.fold_left2 (fun acc e1 e2 -> pairwise_concat acc (collect_unified e1 e2)) ([], []) es1 es2
     | CallE (_, as1), CallE (_, as2) -> collect_unified_args as1 as2
-    | _ -> Util.Error.error e.at "prose transformation" "cannot unify the expression"
+    | _ -> Util.Error.error e.at "prose transformation" "cannot unify the expression with previous rule for the same instruction"
 
 and collect_unified_arg template a = if eq_arg template a then [], [] else match template.it, a.it with
   | ExpA template', ExpA e -> collect_unified template' e
