722f7015ef69e3f96fdf4b09e7cc83f093a8cd39
Wonho <new170527@kaist.ac.kr>
2024-07-29 11:18:09 +0900

    Update error message

--- spectec/src/al/valid.ml
+++ spectec/src/al/valid.ml
@@ -4,16 +4,18 @@ open Ast
 open Print
 open Walk
 
-(* Ignore TODO *)
-let debug = false
 
 (* Error *)
 
 let error at msg = Error.error at "AL validation" msg
-let error_expr expr expected_typ =
+let error_mismatch expr typ1 typ2 =
   error expr.at
-    (Printf.sprintf "%s is expected to be %s but has %s"
-      (string_of_expr expr) expected_typ (Il.Print.string_of_typ expr.note))
+    (Printf.sprintf "type mismatch in %s\n  %s =/= %s"
+      (string_of_expr expr)
+      (Il.Print.string_of_typ typ1)
+      (Il.Print.string_of_typ typ2)
+    )
+let error_expr expr expected_typ = error_mismatch expr expr.note expected_typ
 
 (* Bound Set *)
 
@@ -46,7 +48,6 @@ let is_num typ =
 let rec sub_typ typ1 typ2 =
   match typ1.it, typ2.it with
   | Il.Ast.IterT (typ1', _), Il.Ast.IterT (typ2', _) -> sub_typ typ1' typ2'
-  | _, Il.Ast.VarT ({ it="TODO"; _ }, []) when debug -> true
   | Il.Ast.VarT (id1, _), Il.Ast.VarT (id2, _) ->
     Il.Eval.sub_typ !typ_env (varT id1.it) (varT id2.it) || is_num typ1 && is_num typ2
   | _ ->
@@ -67,47 +68,46 @@ let unwrap_iter_typ typ =
   | Il.Ast.IterT (typ', _) -> typ'
   | _ -> assert (false)
 
-let check_match expr typ =
-  if not (matches expr.note typ) then
-    error_expr expr (Il.Print.string_of_typ typ)
+let check_match expr typ1 typ2 =
+  if not (matches typ1 typ2) then error_mismatch expr typ1 typ2
 
 let check_num expr =
   match expr.note.it with
   | _ when is_num expr.note -> ()
-  | Il.Ast.VarT (id, []) when id.it = "TODO" && debug -> ()
-  | _ -> error_expr expr "num"
+  | _ -> error_expr expr (varT "num")
 
 let check_bool expr =
   match expr.note.it with
   | Il.Ast.BoolT -> ()
-  | Il.Ast.VarT (id, []) when id.it = "TODO" && debug -> ()
-  | _ -> error_expr expr "bool"
+  | _ -> error_expr expr (varT "bool")
 
 let check_list expr =
   match expr.note.it with
   | Il.Ast.IterT (_, iter) when iter <> Il.Ast.Opt -> ()
-  | _ -> error_expr expr "list"
+  | _ -> error_expr expr (varT "list")
 
 let check_instr expr =
   if not (sub_typ (get_base_typ expr.note) (varT "instr")) then
-    error_expr expr "instr"
+    error_expr expr (varT "instr")
 
 let check_val expr =
   if not (sub_typ (get_base_typ expr.note) (varT "val")) then
-    error_expr expr "val"
+    error_expr expr (varT "val")
 
 let check_context expr =
   let context_typs = [ "call frame"; "label" ] in
   match expr.note.it with
   | Il.Ast.VarT (id, []) when List.mem id.it context_typs -> ()
-  | _ -> error_expr expr "context"
+  | _ -> error_expr expr (varT "context")
 
 let check_access expr1 expr2 path =
   match path.it with
   | IdxP expr3 ->
-    check_list expr2; check_num expr3; check_match expr1 (unwrap_iter_typ expr2.note)
+    check_list expr2; check_num expr3;
+    check_match expr1 expr1.note (unwrap_iter_typ expr2.note)
   | SliceP (expr3, expr4) ->
-    check_list expr2; check_num expr3; check_num expr4; check_match expr1 expr2.note
+    check_list expr2; check_num expr3; check_num expr4;
+    check_match expr1 expr1.note expr2.note
   | DotP atom ->
     let field = string_of_atom atom in
     let f =
@@ -116,14 +116,14 @@ let check_access expr1 expr2 path =
     (match expr2.note.it with
     | TupT etl when List.exists f etl ->
       let (_, typ) = List.find f etl in
-      check_match expr1 typ
+      check_match expr1 expr1.note typ
     | VarT (id, _) when Il.Eval.Map.mem id.it !typ_env.typs ->
       let f = fun (atom, _, _) -> Il.Print.string_of_atom atom = field in
       (match Il.Eval.Map.find id.it !typ_env.typs with
       | [{ it = InstD (_, _, { it = StructT tfs; _ }); _ }]
       when List.exists f tfs ->
         let _, (_, typ, _), _ = List.find f tfs in
-        check_match expr1 typ
+        check_match expr1 expr1.note typ
       | _ ->
         error expr2.at
           (Printf.sprintf "%s whose type is %s doesn't contain field %s"
@@ -158,24 +158,24 @@ let valid_expr (walker: unit_walker) (expr: expr) : unit =
   | BinE ((EqOp|NeOp), expr1, expr2) ->
     check_bool expr;
     (* XXX: Not sure about this rule *)
-    check_match expr1 expr2.note
+    check_match expr1 expr1.note expr2.note
   | AccE (expr', path) -> check_access expr expr' path
   (* TODO *)
   | IterE (expr1, _, iter) ->
     let iterT typ iter' = Il.Ast.IterT (typ, iter') $ no_region in
     (match iter with
     | Opt ->
-      check_match expr (iterT expr1.note Il.Ast.Opt);
+      check_match expr expr.note (iterT expr1.note Il.Ast.Opt);
     | ListN (expr2, id_opt) ->
       Option.iter add_bound_var id_opt;
-      check_match expr (iterT expr1.note Il.Ast.List);
+      check_match expr expr.note (iterT expr1.note Il.Ast.List);
       check_num expr2
     | _ ->
-      check_match expr (iterT expr1.note Il.Ast.List);
+      check_match expr expr.note (iterT expr1.note Il.Ast.List);
     )
   | _ ->
     match expr.note.it with
-    | Il.Ast.VarT (id, []) when id.it = "TODO" && not debug ->
+    | Il.Ast.VarT (id, []) when id.it = "TODO" ->
       error expr.at (Printf.sprintf "%s's type is TODO" (string_of_expr expr))
     | _ -> ()
   );
@@ -194,13 +194,13 @@ let valid_instr (walker: unit_walker) (instr: instr) : unit =
       not (sub_typ (get_base_typ expr.note) (varT "val")) &&
       not (sub_typ (get_base_typ expr.note) (varT "callframe"))
     then
-      error_expr expr "val"
+      error_expr expr (varT "val")
   | PopI expr | PopAllI expr -> add_bound_vars expr;
     if
       not (sub_typ (get_base_typ expr.note) (varT "val")) &&
       not (sub_typ (get_base_typ expr.note) (varT "callframe"))
     then
-      error_expr expr "val"
+      error_expr expr (varT "val")
   | LetI (expr1, expr2) ->
     add_bound_vars expr1;
     if not (matches expr1.note expr2.note) then
