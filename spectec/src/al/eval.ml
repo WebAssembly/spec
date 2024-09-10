acaff8efd74d3c98388a9b680641687418042be0
Wonho <new170527@kaist.ac.kr>
2024-09-10 15:26:09 +0900

    Rename subst

--- spectec/src/al/eval.ml
+++ spectec/src/al/eval.ml
@@ -70,7 +70,7 @@ let rec is_normal_exp e =
   | StrE efs -> List.for_all (fun (_, e) -> is_normal_exp !e) efs
   | _ -> false
 
-let rec reduce_exp env e : expr =
+let rec reduce_exp s e : expr =
   Debug_log.(log "al.reduce_exp"
     (fun _ -> fmt "%s" (string_of_expr e))
     (fun e' -> fmt "%s" (string_of_expr e'))
@@ -78,7 +78,7 @@ let rec reduce_exp env e : expr =
   match e.it with
   | VarE _ | BoolE _ | NumE _ -> e
   | UnE (op, e1) ->
-    let e1' = reduce_exp env e1 in
+    let e1' = reduce_exp s e1 in
     (match op, e1'.it with
     | NotOp, BoolE b -> BoolE (not b) $> e
     | NotOp, UnE (NotOp, e11) -> e11
@@ -86,8 +86,8 @@ let rec reduce_exp env e : expr =
     | _ -> UnE (op, e1') $> e
     )
   | BinE (op, e1, e2) ->
-    let e1' = reduce_exp env e1 in
-    let e2' = reduce_exp env e2 in
+    let e1' = reduce_exp s e1 in
+    let e2' = reduce_exp s e2 in
     (match op, e1'.it, e2'.it with
     | AndOp, BoolE true, _ -> e2'
     | AndOp, BoolE false, _ -> e1'
@@ -151,49 +151,49 @@ let rec reduce_exp env e : expr =
   | AccE (e1, p) ->
     (match p.it with
     | IdxP e2 ->
-      let e1' = reduce_exp env e1 in
-      let e2' = reduce_exp env e2 in
+      let e1' = reduce_exp s e1 in
+      let e2' = reduce_exp s e2 in
       (match e1'.it, e2'.it with
       | ListE es, NumE i when i < Z.of_int (List.length es) -> List.nth es (Z.to_int i)
       | _ -> AccE (e1', IdxP e2' $ p.at) $> e
       )
     | SliceP (e2, e3) ->
-      let e1' = reduce_exp env e1 in
-      let e2' = reduce_exp env e2 in
-      let e3' = reduce_exp env e3 in
+      let e1' = reduce_exp s e1 in
+      let e2' = reduce_exp s e2 in
+      let e3' = reduce_exp s e3 in
       (match e1'.it, e2'.it, e3'.it with
       | ListE es, NumE i, NumE n when Z.(i + n) < Z.of_int (List.length es) ->
         ListE (Lib.List.take (Z.to_int n) (Lib.List.drop (Z.to_int i) es))
       | _ -> AccE (e1', SliceP (e2', e3') $ p.at)
       ) $> e
     | DotP atom ->
-      let e1' = reduce_exp env e1 in
+      let e1' = reduce_exp s e1 in
       (match e1'.it with
       | StrE efs -> !(snd (List.find (fun (atomN, _) -> El.Atom.eq atomN atom) efs))
       | _ -> AccE (e1', DotP atom $ p.at) $> e
       )
     )
   | UpdE (e1, p, e2) ->
-    let e1' = reduce_exp env e1 in
-    let e2' = reduce_exp env e2 in
-    reduce_path env e1' p
+    let e1' = reduce_exp s e1 in
+    let e2' = reduce_exp s e2 in
+    reduce_path s e1' p
       (fun e' ps -> if ps = [] then e2' else UpdE (e', ps, e2') $> e')
   | ExtE (_e1, _p, _e2, _dir) -> e
     (* TODO
-    let e1' = reduce_exp env e1 in
-    let e2' = reduce_exp env e2 in
-    reduce_path env e1' p
+    let e1' = reduce_exp s e1 in
+    let e2' = reduce_exp s e2 in
+    reduce_path s e1' p
       (fun e' p' ->
         if p'.it = RootP
-        then reduce_exp env (CatE (e', e2') $> e')
+        then reduce_exp s (CatE (e', e2') $> e')
         else ExtE (e', p', e2') $> e'
       )
       *)
-  | StrE efs -> StrE (List.map (reduce_expfield env) efs) $> e
+  | StrE efs -> StrE (List.map (reduce_expfield s) efs) $> e
   | CompE (e1, e2) ->
     (* TODO(4, rossberg): avoid overlap with CatE? *)
-    let e1' = reduce_exp env e1 in
-    let e2' = reduce_exp env e2 in
+    let e1' = reduce_exp s e1 in
+    let e2' = reduce_exp s e2 in
     (match e1'.it, e2'.it with
     | ListE es1, ListE es2 -> ListE (es1 @ es2)
     | OptE None, OptE _ -> e2'.it
@@ -201,13 +201,13 @@ let rec reduce_exp env e : expr =
     | StrE efs1, StrE efs2 ->
       let merge (atom1, e1) (atom2, e2) =
         assert (El.Atom.eq atom1 atom2);
-        (atom1, ref (reduce_exp env (CompE (!e1, !e2) $> !e1)))
+        (atom1, ref (reduce_exp s (CompE (!e1, !e2) $> !e1)))
       in StrE (List.map2 merge efs1 efs2)
     | _ -> CompE (e1', e2')
     ) $> e
   | MemE (e1, e2) ->
-    let e1' = reduce_exp env e1 in
-    let e2' = reduce_exp env e2 in
+    let e1' = reduce_exp s e1 in
+    let e2' = reduce_exp s e2 in
     (match e2'.it with
     | OptE None -> BoolE false
     | OptE (Some e2') when Eq.eq_expr e1' e2' -> BoolE true
@@ -218,21 +218,21 @@ let rec reduce_exp env e : expr =
     | _ -> MemE (e1', e2')
     ) $> e
   | LenE e1 ->
-    let e1' = reduce_exp env e1 in
+    let e1' = reduce_exp s e1 in
     (match e1'.it with
     | ListE es -> NumE (Z.of_int (List.length es))
     | _ -> LenE e1'
     ) $> e
-  | TupE es -> TupE (List.map (reduce_exp env) es) $> e
+  | TupE es -> TupE (List.map (reduce_exp s) es) $> e
   | CallE (id, args) ->
-    let args' = List.map (reduce_arg env) args in
-    (match reduce_call env id args' with
+    let args' = List.map (reduce_arg s) args in
+    (match reduce_call id args' with
     | None -> CallE (id, args') $> e
     | Some e -> e
     )
   | IterE (e1, iterexp) ->
-    let e1' = reduce_exp env e1 in
-    let (iter', xes') as iterexp' = reduce_iterexp env iterexp in
+    let e1' = reduce_exp s e1 in
+    let (iter', xes') as iterexp' = reduce_iterexp s iterexp in
     let ids, es' = List.split xes' in
     if not (List.for_all is_head_normal_exp es') || iter' <= List1 && es' = [] then
       IterE (e1', iterexp') $> e
@@ -245,14 +245,14 @@ let rec reduce_exp env e : expr =
         else if List.for_all Option.is_some eos' then
           let es1' = List.map Option.get eos' in
           let s = List.fold_right2 Subst.add ids es1' Subst.empty in
-          reduce_exp env (Subst.subst_exp s e1')
+          reduce_exp s (Subst.subst_exp s e1')
         else
           IterE (e1', iterexp') $> e
       | List | List1 ->
         let n = List.length (as_list_exp (List.hd es')) in
         if iter' = List || n >= 1 then
           let en = NumE (Z.of_int n) $$ e.at % (Il.Ast.NumT NatT $ e.at) in
-          reduce_exp env (IterE (e1', (ListN (en, None), xes')) $> e)
+          reduce_exp s (IterE (e1', (ListN (en, None), xes')) $> e)
         else
           IterE (e1', iterexp') $> e
       | ListN ({it = NumE n'; _}, ido) ->
@@ -269,42 +269,42 @@ let rec reduce_exp env e : expr =
                 Subst.add id en s
               )
             in Subst.subst_exp s' e1'
-          )) $> e) |> reduce_exp env
+          )) $> e) |> reduce_exp s
         else
           IterE (e1', iterexp') $> e
       | ListN _ ->
         IterE (e1', iterexp') $> e
       )
-  | OptE eo -> OptE (Option.map (reduce_exp env) eo) $> e
-  | ListE es -> ListE (List.map (reduce_exp env) es) $> e
+  | OptE eo -> OptE (Option.map (reduce_exp s) eo) $> e
+  | ListE es -> ListE (List.map (reduce_exp s) es) $> e
   | CatE (e1, e2) ->
-    let e1' = reduce_exp env e1 in
-    let e2' = reduce_exp env e2 in
+    let e1' = reduce_exp s e1 in
+    let e2' = reduce_exp s e2 in
     (match e1'.it, e2'.it with
     | ListE es1, ListE es2 -> ListE (es1 @ es2)
     | OptE None, OptE _ -> e2'.it
     | OptE _, OptE None -> e1'.it
     | _ -> CatE (e1', e2')
     ) $> e
-  | CaseE (op, es) -> CaseE (op, List.map (reduce_exp env) es) $> e
+  | CaseE (op, es) -> CaseE (op, List.map (reduce_exp s) es) $> e
   | SubE _ -> e
   | _ -> e
 
-and reduce_iter env = function
-  | ListN (e, ido) -> ListN (reduce_exp env e, ido)
+and reduce_iter s = function
+  | ListN (e, ido) -> ListN (reduce_exp s e, ido)
   | iter -> iter
 
-and reduce_iterexp env (iter, xes) =
-  (reduce_iter env iter, List.map (fun (id, e) -> id, reduce_exp env e) xes)
+and reduce_iterexp s (iter, xes) =
+  (reduce_iter s iter, List.map (fun (id, e) -> id, reduce_exp s e) xes)
 
-and reduce_expfield env (atom, e) : (atom * expr ref) = (atom, ref (reduce_exp env !e))
+and reduce_expfield s (atom, e) : (atom * expr ref) = (atom, ref (reduce_exp s !e))
 
-and reduce_path env e p f =
+and reduce_path s e p f =
   match Lib.List.split_last_opt p with
   | None -> f e []
   | Some (ps, p') -> match p'.it with
     | IdxP e1 ->
-      let e1' = reduce_exp env e1 in
+      let e1' = reduce_exp s e1 in
       let f' e' p1' =
         match e'.it, e1'.it with
         | ListE es, NumE i when i < Z.of_int (List.length es) ->
@@ -312,21 +312,21 @@ and reduce_path env e p f =
         | _ ->
           f e' (ps @ [IdxP (e1') $> p'])
       in
-      reduce_path env e ps f'
+      reduce_path s e ps f'
     | SliceP (e1, e2) ->
-      let e1' = reduce_exp env e1 in
-      let e2' = reduce_exp env e2 in
+      let e1' = reduce_exp s e1 in
+      let e2' = reduce_exp s e2 in
       let f' e' p1' =
         match e'.it, e1'.it, e2'.it with
         | ListE es, NumE i, NumE n when Z.(i + n) < Z.of_int (List.length es) ->
           let e1' = ListE Lib.List.(take (Z.to_int i) es) $> e' in
           let e2' = ListE Lib.List.(take (Z.to_int n) (drop (Z.to_int i) es)) $> e' in
           let e3' = ListE Lib.List.(drop Z.(to_int (i + n)) es) $> e' in
-          reduce_exp env (CatE (e1', CatE (f e2' p1', e3') $> e') $> e')
+          reduce_exp s (CatE (e1', CatE (f e2' p1', e3') $> e') $> e')
         | _ ->
           f e' (ps @ [SliceP (e1', e2') $> p'])
       in
-      reduce_path env e ps f'
+      reduce_path s e ps f'
     | DotP atom ->
       let f' e' p1' =
         match e'.it with
@@ -336,40 +336,40 @@ and reduce_path env e p f =
         | _ ->
           f e' (ps @ [DotP (atom) $> p'])
       in
-      reduce_path env e ps f'
+      reduce_path s e ps f'
 
-and reduce_arg env a : arg =
+and reduce_arg s a : arg =
   Debug_log.(log "al.reduce_arg"
     (fun _ -> fmt "%s" (string_of_arg a))
     (fun a' -> fmt "%s" (string_of_arg a'))
   ) @@ fun _ ->
   match a.it with
-  | ExpA e -> ExpA (reduce_exp env e) $ a.at
+  | ExpA e -> ExpA (reduce_exp s e) $ a.at
   | TypA _t -> a  (* types are reduced on demand *)
   | DefA _id -> a
   (* | GramA _g -> a *)
 
-and reduce_call _env id args : expr option =
+and reduce_call id args : expr option =
   let func_finder = fun al -> match al.it with | FuncA (fname, _, _) -> fname = id | RuleA _ -> false in
   match (List.find func_finder !Lang.al).it with
   | FuncA (_, params, il) ->
-    let env = List.fold_right2 get_subst_arg params args Subst.empty in
-    reduce_instrs env il
+    let s = List.fold_right2 get_subst_arg params args Subst.empty in
+    reduce_instrs s il
   | _ -> assert (false)
 
-and reduce_instrs env : instr list -> expr option = function
+and reduce_instrs s : instr list -> expr option = function
   | [] -> None
   | instr :: t ->
     match instr.it with
-    | ReturnI expr_opt -> Option.map (reduce_exp env) expr_opt
+    | ReturnI expr_opt -> Option.map (reduce_exp s) expr_opt
     | LetI (expr1, expr2) ->
-      let new_env = get_subst expr1 expr2 Subst.empty in
-      reduce_instrs new_env t
+      let new_s = get_subst expr1 expr2 s in
+      reduce_instrs new_s t
     | IfI (expr, il1, il2) ->
       (* TODO: consider iter *)
-      (match (reduce_exp env expr).it with
-      | BoolE true -> reduce_instrs env (il1@t)
-      | BoolE false -> reduce_instrs env (il2@t)
+      (match (reduce_exp s expr).it with
+      | BoolE true -> reduce_instrs s (il1@t)
+      | BoolE false -> reduce_instrs s (il2@t)
       | _ -> None
       )
     (* Can have side effect *)
@@ -378,4 +378,4 @@ and reduce_instrs env : instr list -> expr option = function
     | EnterI _  | PushI _ | PopI _ | PopAllI _ | TrapI | ThrowI _
     | ExecuteI _ | ExecuteSeqI _ | ExitI _ | OtherwiseI _ | YetI _ -> assert (false)
     (* Nop *)
-    | (AssertI _ | NopI) -> reduce_instrs env t
+    | (AssertI _ | NopI) -> reduce_instrs s t
