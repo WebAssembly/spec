af2ffedfcbab5a8ce671af34c3025c3c3bfef987
sideshowbarker <sideshowbarker@users.noreply.github.com>
2024-09-13 03:14:31 +0000

    deploy: 50429509db9a1cd0f71f8a8b93ded37e4720d4a9

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 (Draft 2024-09-04)',
+    VERSION: '2.0 (Draft 2024-09-13)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
