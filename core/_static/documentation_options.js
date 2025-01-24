787da17cd648ada26048cbb4abfc72372d9d949b
Ms2ger <Ms2ger@users.noreply.github.com>
2025-01-24 10:01:25 +0000

    deploy: 38b5bddffb98d51d28e8fd6e61340fa27daf5add

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 (Draft 2024-12-18)',
+    VERSION: '2.0 (Draft 2025-01-24)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
