3dea84829f6bf83c549d2ab488cf15571ee50a51
Andreas Rossberg <rossberg@mpi-sws.org>
2025-11-19 23:31:16 +0100

    Remove obsolete interface

--- interpreter/script/runner.mli
+++ interpreter/script/runner.mli
@@ -1,11 +1,5 @@
 module Make (Engine : Engine.Engine) :
 sig
-  exception Abort of Source.region * string
-  exception Assert of Source.region * string
-  exception IO of Source.region * string
-
-  val trace : string -> unit
-
   val register_instance : Ast.name -> Engine.moduleinst -> unit
   val register_virtual : Ast.name ->
     (Ast.name -> Types.externtype -> Engine.externinst option) ->
