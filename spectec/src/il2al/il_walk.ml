938f2db9213237fd6ef84127bb376966cc416bdd
Joachim Breitner <mail@joachim-breitner.de>
2025-12-01 12:20:53 +0100

    Add IfE constuctor to IL AST (#210)

--- spectec/src/il2al/il_walk.ml
+++ spectec/src/il2al/il_walk.ml
@@ -52,6 +52,7 @@ let rec transform_exp t e =
     | MemE (e1, e2) -> MemE (t_exp e1, t_exp e2)
     | CaseE (mixop, e1) -> CaseE (mixop, t_exp e1)
     | SubE (e1, _t1, t2) -> SubE (t_exp e1, _t1, t2)
+    | IfE (e1, e2, e3) -> IfE (t_exp e1, t_exp e2, t_exp e3)
   in
   f { e with it }
 
